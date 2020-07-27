f52709c656851ea7f3efa7af13be747bdc03c7e8
marcellus <War13win23@>
2020-07-27 18:27:37 +0200

    Adding Category model, repository and CategoryDTO

---
+++ src/main/java/com/marcellus/spring5rest/api/v1/model/CategoryDTO.java
@@ -0,0 +1,7 @@
+package com.marcellus.spring5rest.api.v1.model;
+
+public class CategoryDTO {
+
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/marcellus/spring5rest/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.marcellus.spring5rest.repositories;
+
+import com.marcellus.spring5rest.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
