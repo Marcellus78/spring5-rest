1f92d5747fb92b833ebfdd283def518227728dc2
marcellus <War13win23@>
2020-07-27 18:46:24 +0200

    fixing Category annotation

--- src/main/java/com/marcellus/spring5rest/domain/Category.java
+++ src/main/java/com/marcellus/spring5rest/domain/Category.java
@@ -1,12 +1,15 @@
 package com.marcellus.spring5rest.domain;
 
-import lombok.Data;
-import org.springframework.data.annotation.Id;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
+@Entity
 public class Category {
 
     @Id
